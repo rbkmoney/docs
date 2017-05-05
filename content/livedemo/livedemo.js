62752df75e4218b1b23fb941f4a453bc03f152f9
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-05-05 18:06:42 +0300

    FE-310: fix multiline string for IE

--- content/livedemo/livedemo.js
+++ content/livedemo/livedemo.js
@@ -1,36 +1,35 @@
 const $liveDemo = document.getElementById('live-demo');
 if($liveDemo) {
 
-    $liveDemo.innerHTML = `
-        <div id="live-demo-container">
-            <form action='javascript:initInvoice();'>
-                <h2>Конфигурация товара</h2>
-                <label class='form-row'>
-                    Наименование
-                    <input type='text' id='product' value='Резиновая уточка' />
-                </label>
-                <label class='form-row'>
-                    Описание
-                    <input type='text' id='description' value='Очень нужная вещь' />
-                </label>
-                <label class='form-row'>
-                    Стоимость
-                    <input type='number' id='amount' min='1' max='40000' value='1499' />
-                </label>
-                <input type='submit' value='Создать Invoice в системе' />
-            </form>
-        </div>
-        <div id="fountainG" style="display: none">
-            <div id="fountainG_1" class="fountainG"></div>
-            <div id="fountainG_2" class="fountainG"></div>
-            <div id="fountainG_3" class="fountainG"></div>
-            <div id="fountainG_4" class="fountainG"></div>
-            <div id="fountainG_5" class="fountainG"></div>
-            <div id="fountainG_6" class="fountainG"></div>
-            <div id="fountainG_7" class="fountainG"></div>
-            <div id="fountainG_8" class="fountainG"></div>
-        </div>
-    `;
+    $liveDemo.innerHTML =
+        '<div id="live-demo-container">' +
+            '<form action="javascript:initInvoice();">' +
+                '<h2>Конфигурация товара</h2>' +
+                '<label class="form-row">' +
+                    'Наименование' +
+                    '<input type="text" id="product" value="Резиновая уточка" />' +
+                '</label>' +
+                '<label class="form-row">' +
+                    'Описание' +
+                    '<input type="text" id="description" value="Очень нужная вещь" />' +
+                '</label>' +
+                '<label class="form-row">' +
+                    'Стоимость' +
+                    '<input type="number" id="amount" min="1" max="40000" value="1499" />' +
+                '</label>' +
+                '<input type="submit" value="Создать Invoice в системе" />' +
+            '</form>' +
+        '</div>' +
+        '<div id="fountainG" style="display: none">' +
+            '<div id="fountainG_1" class="fountainG"></div>' +
+            '<div id="fountainG_2" class="fountainG"></div>' +
+            '<div id="fountainG_3" class="fountainG"></div>' +
+            '<div id="fountainG_4" class="fountainG"></div>' +
+            '<div id="fountainG_5" class="fountainG"></div>' +
+            '<div id="fountainG_6" class="fountainG"></div>' +
+            '<div id="fountainG_7" class="fountainG"></div>' +
+            '<div id="fountainG_8" class="fountainG"></div>' +
+        '</div>';
 
     var $container = document.getElementById('live-demo-container');
     var $loader = document.getElementById('fountainG');
@@ -71,7 +70,7 @@ if($liveDemo) {
                 }
             }
         };
-        request.open('POST', `${backendOriginUrl}/invoice/create`, true);
+        request.open('POST', backendOriginUrl + '/invoice/create', true);
         request.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
         request.send(JSON.stringify(invoiceArgs));
     }
@@ -88,7 +87,7 @@ if($liveDemo) {
                 }
             }
         };
-        request.open('POST', `${backendOriginUrl}/invoice/${invoice.id}/access_tokens`, true);
+        request.open('POST', backendOriginUrl + '/invoice/' + invoice.id + '/access_tokens', true);
         request.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
         request.send();
     }
@@ -102,14 +101,13 @@ if($liveDemo) {
 
     function initPayButton(invoice, payload) {
         var amountMajor = Number(invoice.amount) / 100;
-        $container.innerHTML = `
-            <div id="payment">
-                <h2 class="order" data-title="ID запроса: ${invoice.id}">Сформирован запрос</h2>
-                <div>Товар: ${invoice.product}</div>
-                <div>Сумма к оплате: ${amountMajor} ${invoice.currency}</div>
-                <button class="checkout-button" onclick="openCheckout()">Оплатить</button>
-            </div>
-        `;
+        $container.innerHTML =
+            '<div id="payment">' +
+                '<h2 class="order" data-title="ID запроса: ' + invoice.id + '">Сформирован запрос</h2>' +
+                '<div>Товар: ' + invoice.product + '</div>' +
+                '<div>Сумма к оплате: ' + amountMajor + ' ' + invoice.currency + '</div>' +
+                '<button class="checkout-button" onclick="openCheckout()">Оплатить</button>' +
+            '</div>';
 
         checkout = RbkmoneyCheckout.configure({
             invoiceID: invoice.id,
